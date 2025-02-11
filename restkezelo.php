b14f7f3d4ce22fbf542e7830e2ae8fca5aa7fbf4
Leventeszily <72456857620@szily.hu>
2025-02-11 11:55:05 +0100

    restkezelo.php elkeszült

---
+++ restkezelo.php
@@ -0,0 +1,27 @@
+<?php
+
+class Restkezelo
+{
+    private $httpversion = "http/1.1";
+    
+    public function sethttpfejlec($statuskod)
+    {
+        $statusuzenet=$this->gethttpstatusuzenet($statuskod);
+
+        header($tihs->httpversion. " " . $statuskod . " " . $statusuzenet);
+        header("content-Type: application/json");
+    }
+
+    public function gethttpstatusuzenet($statuskod)
+    {
+        $httpstatus = array(
+            200 -> 'OK'
+            400 -> 'Bad Request'
+            404 -> 'Not Found'
+            500 -> 'Internal Server Error'
+        );
+        return($httpstatus[$statuskod]) ? $httpstatus[$statuskod] : $httpstatus[500];
+    }
+}
+
+?>
